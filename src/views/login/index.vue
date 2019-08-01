ec8dca2f161afd03dad7db9f475d2e78a8b91493
169 <169@qq.com>
2019-08-01 22:30:25 +0800

    login基础页面

---
+++ src/views/login/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <el-card class="box-card">
+      <img src="../../assets/images/logo_index.png" alt />
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped  lang="less">
+.container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+  .box-card {
+    position: absolute;
+    width: 400px;
+    height: 350px;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      display: block;
+      width: 200px;
+      margin: 0 auto;
+    }
+  }
+}
+</style>
