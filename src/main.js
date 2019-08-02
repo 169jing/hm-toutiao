108c732786349864a89ca3bc6b2ff8f8991c033a
169 <169@qq.com>
2019-08-02 21:21:45 +0800

    登录页面&跳转首页

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
 import '@/style/index.less'
+import axios from 'axios'
+Vue.prototype.$http = axios
 
 Vue.use(ElementUI)
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Login from '@/views/login'
+import Home from '@/views/home'
+import Welcome from '@/views/welcome'
 
 Vue.use(VueRouter)
 
@@ -8,7 +10,19 @@ const router = new VueRouter({
   routes: [
     {
       path: '/login', name: 'login', component: Login
+
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      children: [
+
+        { path: '/', name: 'welcome', component: Welcome }
+
+      ]
     }
+
   ]
 })
 export default router
